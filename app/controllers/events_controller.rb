728d73271874e59217c3ce617c3e0aa828cf9fd9
Alexandra DLG <adelagueronniere@gmail.com>
2018-05-03 15:32:38 +0200

    first commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,24 @@
+class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.creator_id = current_user.id
+    @event.save
+    redirect_to user_path(current_user)
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  
+  def index
+    @events = Event.all
+  end
+
+  def event_params
+    params.require(:event).permit(:description, :date, :place)
+  end
+end
---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    description "MyText"
+    date "2018-05-03 11:16:12"
+    place "MyString"
+  end
+end
